59bf08ac954c3dfe787f94845c6c31b2d017b65b
Mor Paz <1912.mor@gmail.com>
2022-06-24 11:49:36 +0300

    Update parsing of executer messages for robots Add mock publisher package

--- src/crl_executer/crl_executer/executer.py
+++ src/crl_executer/crl_executer/executer.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Pose, Twist
 from goal_status_interface.msg import GoalStatus
 from robot_status_interface.msg import RobotStatus
 from robot_path_assignment_interface.msg import RobotPathAssignment
@@ -8,6 +8,10 @@ from robot_path_assignment_interface.msg import RobotPathAssignment
 MAPF_PLAN_TOPIC = '/MAPF_PLAN'
 ROBOT_STATUS_TOPIC = '/ROBOT_STATUS'
 GOAL_STATUS_TOPIC = '/GOAL_STATUS'
+
+BASE_ROBOT_TOPIC = '/robot/'
+
+ROBOT_CACHE = {}
 
 
 class Executer(Node):
@@ -26,7 +30,6 @@ class Executer(Node):
 
     def expose_goal_status_topic(self):
         self.goal_status_topic = self.create_publisher(GoalStatus, GOAL_STATUS_TOPIC, 10)
-
 
     # TODO: need to decide how to publish messages from both of these events, the format might be differnet, maybe we want to
     # publish from gazebo to virtual robots only, and the same from optiTrack to physical?
@@ -81,20 +84,64 @@ class Executer(Node):
         pass
 
     def send_plan_to_robot(self, msg):
+        # Assumptions
+        # 1. The first task assignment for a robot must be START
+        # 2. The first pose stamped in the first task assignment is the initial location of the robot
         # TODO: figure out how to correctly send plans to robots (NAV2/TWIST/something else?)
         target_robot_id = msg.target_robot_id
         task = msg.task
         if task == 'STOP':
+            self.get_logger().warn('Received STOP!')
             # Stop robot immediately
             pass
         elif task == 'START':
+            self.get_logger().warn('Received START!')
+            if target_robot_id not in ROBOT_CACHE:
+                # This is the first message about this robot, and the robot has no initial position, so the first pose stamped is the robots' initial position
+                self._save_robot_initial_position(target_robot_id, msg.path[0])
             # Tell robot to stop performing current tasks and begin a new path
-            pass
+            self._send_twist_message(0)
+            self._send_twist_message_1(1)
         elif task == 'EXTEND':
+            self.get_logger().warn('Received EXTEND!')
             # Tell the robot to continue with these added assignments after it is done with current tasks
             pass
-        # Find the correct robot and send him the task
-        pass
+
+    def _send_twist_message(self, robot_id):
+        self.get_logger().warn('Sending twist message!')
+        cmd_vel_pub = self.create_publisher(Twist, f'/robot{robot_id}/cmd_vel', 10)
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.1
+        twist_msg.linear.y = 0.0
+        twist_msg.linear.z = 0.0
+        twist_msg.angular.x = 0.1
+        twist_msg.angular.y = 0.0
+        twist_msg.angular.z = 0.0
+        cmd_vel_pub.publish(twist_msg)
+
+    def _send_twist_message_1(self, robot_id):
+        self.get_logger().warn('Sending twist message!')
+        cmd_vel_pub = self.create_publisher(Twist, f'/robot{robot_id}/cmd_vel', 10)
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.5
+        twist_msg.linear.y = 0.1
+        twist_msg.linear.z = 0.0
+        twist_msg.angular.x = 0.1
+        twist_msg.angular.y = 0.1
+        twist_msg.angular.z = 0.0
+        cmd_vel_pub.publish(twist_msg)
+
+    def _save_robot_initial_position(self, robot_id, pose_stamped: PoseStamped):
+        initial_pose = Pose()
+        initial_pose.position = pose_stamped.pose.position
+        initial_pose.orientation = pose_stamped.pose.orientation
+        ROBOT_CACHE[robot_id] = initial_pose
+
+    def _save_robot_current_position(self, robot_id, pose_stamped: PoseStamped):
+        initial_pose = Pose()
+        initial_pose.position = pose_stamped.pose.position
+        initial_pose.orientation = pose_stamped.pose.orientation
+        ROBOT_CACHE[robot_id] = initial_pose
 
 
 def main(args=None):
---
+++ src/mock_publisher/mock_publisher/publisher.py
@@ -0,0 +1,55 @@
+import time
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped, Quaternion
+from goal_status_interface.msg import GoalStatus
+from robot_status_interface.msg import RobotStatus
+from robot_path_assignment_interface.msg import RobotPathAssignment
+
+
+class MockPublisher(Node):
+    msg_publisher = None
+
+    def __init__(self):
+        super().__init__('MockPublisher')
+        self.msg_publisher = self.create_publisher(RobotPathAssignment, '/MAPF_PLAN', 10)
+        # self.publish_mock_message_to_topic()
+        timer_period = 1
+        self.timer = self.create_timer(timer_period, self.publish_mock_message_to_topic)
+
+    def publish_mock_message_to_topic(self):
+        message = RobotPathAssignment()
+        message.target_robot_id = 0
+        message.path.append(self.construct_pose_stamped(2.0,3.0,0.1))
+        time.sleep(1)
+        message.path.append(self.construct_pose_stamped(2.5, 3.0, 0.1))
+        message.task = 'START'
+
+        self.msg_publisher.publish(message)
+
+
+    def construct_pose_stamped(self, px, py, pz ):
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = self.get_clock().now().to_msg()
+        pose.pose.position.x = px
+        pose.pose.position.y = py
+        pose.pose.position.z = pz
+        pose.pose.orientation.x = 1.0
+        pose.pose.orientation.y = 1.0
+        pose.pose.orientation.z = 1.0
+        pose.pose.orientation.w = 1.0
+
+        return pose
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MockPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
